e88c57d542d6bf0da94a9ea9aadecec901fca739
Charles Strahan <charles.c.strahan@gmail.com>
2011-01-19 08:37:47 -0600

    split Recursive into RecursiveAction and RecursiveFunc, added Lambda.Y overloads.

--- src/CStrahan/Lambda.cs
+++ src/CStrahan/Lambda.cs
@@ -6,43 +6,75 @@ namespace CStrahan
     {
         static Action Y(Func<Action, Action> f)
         {
-            Recursive rec = r => () => f(r(r))();
+            RecursiveAction rec = r => () => f(r(r))();
+            return rec(rec);
+        }
+        
+        static Action<TArg1> Y<TArg1>(Func<Action<TArg1>, Action<TArg1>> f)
+        {
+            RecursiveAction<TArg1> rec = r => arg1 => f(r(r))(arg1);
             return rec(rec);
         }
 
+        static Action<TArg1, TArg2> Y<TArg1, TArg2>(Func<Action<TArg1, TArg2>, Action<TArg1, TArg2>> f)
+        {
+            RecursiveAction<TArg1, TArg2> rec = r => (arg1, arg2) => f(r(r))(arg1, arg2);
+            return rec(rec);
+        }
+
+        static Action<TArg1, TArg2, TArg3> Y<TArg1, TArg2, TArg3>(Func<Action<TArg1, TArg2, TArg3>, Action<TArg1, TArg2, TArg3>> f)
+        {
+            RecursiveAction<TArg1, TArg2, TArg3> rec = r => (arg1, arg2, arg3) => f(r(r))(arg1, arg2, arg3);
+            return rec(rec);
+        }
+
+        static Action<TArg1, TArg2, TArg3, TArg4> Y<TArg1, TArg2, TArg3, TArg4>(Func<Action<TArg1, TArg2, TArg3, TArg4>, Action<TArg1, TArg2, TArg3, TArg4>> f)
+        {
+            RecursiveAction<TArg1, TArg2, TArg3, TArg4> rec = r => (arg1, arg2, arg3, arg4) => f(r(r))(arg1, arg2, arg3, arg4);
+            return rec(rec);
+        }
+
+        static Action<TArg1, TArg2, TArg3, TArg4, TArg5> Y<TArg1, TArg2, TArg3, TArg4, TArg5>(Func<Action<TArg1, TArg2, TArg3, TArg4, TArg5>, Action<TArg1, TArg2, TArg3, TArg4, TArg5>> f)
+        {
+            RecursiveAction<TArg1, TArg2, TArg3, TArg4, TArg5> rec = r => (arg1, arg2, arg3, arg4, arg5) => f(r(r))(arg1, arg2, arg3, arg4, arg5);
+            return rec(rec);
+        }
+
+        
+
         static Func<TReturn> Y<TReturn>(Func<Func<TReturn>, Func<TReturn>> f)
         {
-            Recursive<TReturn> rec = r => () => f(r(r))();
+            RecursiveFunc<TReturn> rec = r => () => f(r(r))();
             return rec(rec);
         }
 
         static Func<TArg1, TReturn> Y<TArg1, TReturn>(Func<Func<TArg1, TReturn>, Func<TArg1, TReturn>> f)
         {
-            Recursive<TArg1, TReturn> rec = r => arg1 => f(r(r))(arg1);
+            RecursiveFunc<TArg1, TReturn> rec = r => arg1 => f(r(r))(arg1);
             return rec(rec);
         }
 
         static Func<TArg1, TArg2, TReturn> Y<TArg1, TArg2, TReturn>(Func<Func<TArg1, TArg2, TReturn>, Func<TArg1, TArg2, TReturn>> f)
         {
-            Recursive<TArg1, TArg2, TReturn> rec = r => (arg1, arg2) => f(r(r))(arg1, arg2);
+            RecursiveFunc<TArg1, TArg2, TReturn> rec = r => (arg1, arg2) => f(r(r))(arg1, arg2);
             return rec(rec);
         }
 
         static Func<TArg1, TArg2, TArg3, TReturn> Y<TArg1, TArg2, TArg3, TReturn>(Func<Func<TArg1, TArg2, TArg3, TReturn>, Func<TArg1, TArg2, TArg3, TReturn>> f)
         {
-            Recursive<TArg1, TArg2, TArg3, TReturn> rec = r => (arg1, arg2, arg3) => f(r(r))(arg1, arg2, arg3);
+            RecursiveFunc<TArg1, TArg2, TArg3, TReturn> rec = r => (arg1, arg2, arg3) => f(r(r))(arg1, arg2, arg3);
             return rec(rec);
         }
 
         static Func<TArg1, TArg2, TArg3, TArg4, TReturn> Y<TArg1, TArg2, TArg3, TArg4, TReturn>(Func<Func<TArg1, TArg2, TArg3, TArg4, TReturn>, Func<TArg1, TArg2, TArg3, TArg4, TReturn>> f)
         {
-            Recursive<TArg1, TArg2, TArg3, TArg4, TReturn> rec = r => (arg1, arg2, arg3, arg4) => f(r(r))(arg1, arg2, arg3, arg4);
+            RecursiveFunc<TArg1, TArg2, TArg3, TArg4, TReturn> rec = r => (arg1, arg2, arg3, arg4) => f(r(r))(arg1, arg2, arg3, arg4);
             return rec(rec);
         }
 
         static Func<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn> Y<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn>(Func<Func<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn>, Func<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn>> f)
         {
-            Recursive<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn> rec = r => (arg1, arg2, arg3, arg4, arg5) => f(r(r))(arg1, arg2, arg3, arg4, arg5);
+            RecursiveFunc<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn> rec = r => (arg1, arg2, arg3, arg4, arg5) => f(r(r))(arg1, arg2, arg3, arg4, arg5);
             return rec(rec);
         }
     }
--- src/CStrahan/Recursive.cs
+++ src/CStrahan/Recursive.cs
@@ -2,11 +2,17 @@ using System;
 
 namespace CStrahan
 {
-    delegate Action Recursive(Recursive r);
-    delegate Func<TReturn> Recursive<TReturn>(Recursive<TReturn> r);
-    delegate Func<TArg1, TReturn> Recursive<TArg1, TReturn>(Recursive<TArg1, TReturn> r);
-    delegate Func<TArg1, TArg2, TReturn> Recursive<TArg1, TArg2, TReturn>(Recursive<TArg1, TArg2, TReturn> r);
-    delegate Func<TArg1, TArg2, TArg3, TReturn> Recursive<TArg1, TArg2, TArg3, TReturn>(Recursive<TArg1, TArg2, TArg3, TReturn> r);
-    delegate Func<TArg1, TArg2, TArg3, TArg4, TReturn> Recursive<TArg1, TArg2, TArg3, TArg4, TReturn>(Recursive<TArg1, TArg2, TArg3, TArg4, TReturn> r);
-    delegate Func<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn> Recursive<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn>(Recursive<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn> r);
+    delegate Action RecursiveAction(RecursiveAction r);
+    delegate Action<TArg1> RecursiveAction<TArg1>(RecursiveAction<TArg1> r);
+    delegate Action<TArg1, TArg2> RecursiveAction<TArg1, TArg2>(RecursiveAction<TArg1, TArg2> r);
+    delegate Action<TArg1, TArg2, TArg3> RecursiveAction<TArg1, TArg2, TArg3>(RecursiveAction<TArg1, TArg2, TArg3> r);
+    delegate Action<TArg1, TArg2, TArg3, TArg4> RecursiveAction<TArg1, TArg2, TArg3, TArg4>(RecursiveAction<TArg1, TArg2, TArg3, TArg4> r);
+    delegate Action<TArg1, TArg2, TArg3, TArg4, TArg5> RecursiveAction<TArg1, TArg2, TArg3, TArg4, TArg5>(RecursiveAction<TArg1, TArg2, TArg3, TArg4, TArg5> r);
+
+    delegate Func<TReturn> RecursiveFunc<TReturn>(RecursiveFunc<TReturn> r);
+    delegate Func<TArg1, TReturn> RecursiveFunc<TArg1, TReturn>(RecursiveFunc<TArg1, TReturn> r);
+    delegate Func<TArg1, TArg2, TReturn> RecursiveFunc<TArg1, TArg2, TReturn>(RecursiveFunc<TArg1, TArg2, TReturn> r);
+    delegate Func<TArg1, TArg2, TArg3, TReturn> RecursiveFunc<TArg1, TArg2, TArg3, TReturn>(RecursiveFunc<TArg1, TArg2, TArg3, TReturn> r);
+    delegate Func<TArg1, TArg2, TArg3, TArg4, TReturn> RecursiveFunc<TArg1, TArg2, TArg3, TArg4, TReturn>(RecursiveFunc<TArg1, TArg2, TArg3, TArg4, TReturn> r);
+    delegate Func<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn> RecursiveFunc<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn>(RecursiveFunc<TArg1, TArg2, TArg3, TArg4, TArg5, TReturn> r);
 }
